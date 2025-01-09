ca9bb129508cd53c08c14eb2818154d9b3ef6031
jjguillen <josejavierguillen@gmail.com>
2025-01-09 16:13:30 +0100

    Inicio

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return "Usuarios - admin";
+    })->name('users');
+
+    Route::get('/users/{id}', function ($id) {
+        return "Id: ".$id. " - admin";
+    })->whereNumber('id')->name("miruta");
+
+    //Todas las rutas del recurso eventos
+    Route::resource('eventos', EventoController::class);
+});
+
+Route::prefix('web')->group(function () {
+    Route::get('/users', function () {
+        return "Usuarios - web";
+    })->name('users');
+
+    Route::get('/users/{id}', function ($id) {
+        return "Id: ".$id. " - web";
+    })->whereNumber('id')->name("miruta");
+
+    //Solo añadimos las rutas de eventos de ver todos y ver uno solo
+    Route::get('/eventos', [EventoController::class, 'index']);
+    Route::get('/eventos/{evento}', [EventoController::class, 'show']);
+    Route::get('/eventos/id/{id}', [EventoController::class, 'ver']);
+
+});
+
+
+
